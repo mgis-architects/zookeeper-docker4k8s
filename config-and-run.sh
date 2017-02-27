0b904fed5ba0fa96af48f6b11e393532335365dc
Dubnik <ADU089@crb.apmoller.net>
2017-02-27 12:58:28 +0000

    remove debug data

--- config-and-run.sh
+++ config-and-run.sh
@@ -16,7 +16,6 @@ if [ ! -z "$MAX_SERVERS" ]; then
   echo "" >> /opt/zookeeper/conf/zoo.cfg
   echo "#Server List" >> /opt/zookeeper/conf/zoo.cfg
   for i in $( eval echo {1..$MAX_SERVERS});do
-    echo $i serverlist[$(hostname)]
     if [ "${i}" -eq ${serverlist[$(hostname)]} ];then
       echo "server.$i=0.0.0.0:2888:3888" >> /opt/zookeeper/conf/zoo.cfg
     else
